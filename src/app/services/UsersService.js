04668f0bf3016edd329797265c1818914c0e8083
lorenzo-pachioli <lorenzopachioli@gmail.com>
2023-01-09 16:44:47 -0300

    User re factored

--- src/app/services/UsersService.js
+++ src/app/services/UsersService.js
@@ -1,64 +1,53 @@
 const UsersBrainstorming = require('../models/UsersModel');
 const { createUsers } = require('../helpers/modelCreators');
-const { response } = require('../../utils/response');
 const { remover } = require('../../utils/remover');
+const { serviceReturn } = require('../../utils/response');
 
-exports.NewUsersService = async (newUser, res) => {
+exports.NewUsersService = async (newUser) => {
 
   const userAlreadyExist = await UsersBrainstorming.findOne({ username: newUser.username });
-  if (userAlreadyExist) return response(`Username '${newUser.username}' already exist`, res, 200, {});
-
+  if (userAlreadyExist) return serviceReturn(`Username '${newUser.username}' already exist`, {}, false);
   const userList = await UsersBrainstorming.findOne().sort({ _id: -1 }).limit(1);
   const newMaxId = userList.id + 1;
   const user = createUsers(newMaxId, newUser);
-  user.save();
-
-  return response(`User created succesfully`, res, 200, user);
+  const saved = await user.save();
+  if (saved) return serviceReturn(`User created succesfully`, saved, true);
+  return serviceReturn(`The user ${newStory.id} failed to save`, {}, false);
 }
 
-exports.AllUsersService = async (res) => {
+exports.AllUsersService = async () => {
 
   const userList = await UsersBrainstorming.find();
   if (userList.length > 0) {
     const userListWithoutPass = remover(userList, 'password');
-    return response(`User list`, res, 200, userListWithoutPass);
+    return serviceReturn(`User list`, userListWithoutPass, true);
   }
-
-  return response(`User list is empty`, res, 200);
+  return serviceReturn(`User list is empty`, [], true);
 }
 
-exports.UsersServiceById = async (_id, res) => {
+exports.UsersServiceById = async (_id) => {
 
   const user = await UsersBrainstorming.findOne({ _id });
-
   if (user) {
     const userWithoutPassword = remover(user, 'password');
-    return response(`User ${_id}`, res, 200, userWithoutPassword);
+    return serviceReturn(`User ${_id}`, userWithoutPassword, true);
   }
-
-  return response(`User ${_id} doesn't exist`, res, 200, {});
+  return serviceReturn(`User ${_id} doesn't exist`, {}, false);
 }
 
-exports.ModifyUsersService = async (newUser, res) => {
-
-  const user = await UsersBrainstorming.findOne({ id: newUser.id });
-  if (user.password !== newUser.password) return response(`Incorrect user password`, res, 200, {});
+exports.ModifyUsersService = async (newUser) => {
 
   const userUpdated = await UsersBrainstorming.findOneAndUpdate({ _id: newUser._id }, newUser, { new: true });
-
   if (userUpdated) {
     const userUpdatedNoPass = remover(userUpdated);
-    return response(`User ${newUser.id} updated`, res, 200, userUpdatedNoPass);
+    return serviceReturn(`User ${newUser.id} updated`, userUpdatedNoPass, true);
   }
-
-  return response(`Couldn't update user`, res, 200, {});
+  return serviceReturn(`Couldn't update user`, {}, false);
 }
 
-exports.UserDeleteByIdService = async (_id, res) => {
+exports.UserDeleteByIdService = async (_id) => {
 
-  const user = await UsersBrainstorming.findOne({ _id });
   const userById = await UsersBrainstorming.deleteOne({ _id });
-  if (userById.deletedCount > 0) return response(`user ${_id}`, res, 200, user);
-
-  return response(`user ${_id} doesn't exist`, res, 200, {});
+  if (userById.deletedCount > 0) return serviceReturn(`user ${_id}`, {}, true);
+  return serviceReturn(`user ${_id} doesn't exist`, {}, false);
 }
